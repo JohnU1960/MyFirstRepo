7f5cfc0871528949bfeebfd4cb9d98752f828c40
John Ulicny <johnulicny18@gmail.com>
2024-01-31 13:51:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#John Ulicny's code submission
+This is my first attempt at submitting R sample code.
---
+++ rsample.R
@@ -0,0 +1,22 @@
+
+
+
+cancelled <- flights %>%
+  filter(is.na(dep_delay), is.na(arr_delay))
+
+canperd <- cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarize(n_perday = n(),
+            avg_delay = mean(arr_delay))
+apropos("attribute")
+attributes(canperd)$groups
+colnames(mpg)
+table(mpg$trans)
+table(mpg$drv)
+mpg_gr <- mpg %>% group_by(drv,cyl)
+attributes(mpg_gr)$groups
+ungroup(mpg_gr)
+attributes(mpg_gr)
+mpg_gr %>% mutate(grpavg=mean(displ), .keep="used") %>% 
+  head(10)
+str(mpg_gr)
